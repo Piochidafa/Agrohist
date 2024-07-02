5865871d62a4a39ea36b2169909e0e1991f0897c
piochidafa <cauacastrooliveira@outlook.com>
2024-07-02 20:28:08 -0300

    Subindo proj

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import quato from './assets/Images/quato.jpg'
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import './App.css'
+import Notfund from './pages/notfund';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <PrimeReactProvider>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>SEXO</h1>}/>
+          <Route path='*' element={<Notfund/>}/>
+        </Routes>
+    </BrowserRouter>
+    </PrimeReactProvider>
+  )
+}
+
+export default App
---
+++ src/pages/Notfund.jsx
@@ -0,0 +1,11 @@
+import { Image } from "primereact/image"
+import quato from "../assets/Images/quato.jpg"
+
+const Notfund = () => {
+
+    <div className=" w-screen ">
+        <Image src="../assets/Images/quato.jpg" />
+    </div>
+
+}
+export default Notfund
